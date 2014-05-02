ab93f617677a202423ff377c62e50e03ce5ca248
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-05-02 09:35:38 +0200

    Adding statuscode when output exception

--- app/config/error_reporting.php
+++ app/config/error_reporting.php
@@ -9,7 +9,7 @@
  *
  */
 error_reporting(-1);              // Report all type of errors
-ini_set('display_errors', 1);     // Display all errors 
+ini_set('display_errors', 1);     // Display all errors
 ini_set('output_buffering', 0);   // Do not buffer outputs, write directly
 
 
@@ -17,7 +17,12 @@ ini_set('output_buffering', 0);   // Do not buffer outputs, write directly
  * Default exception handler.
  *
  */
-set_exception_handler(function($e) {
-    echo "Anax: Uncaught exception: <p>" . $e->getMessage() . "</p><pre>" . $e->getTraceAsString(), "</pre>";
+set_exception_handler(function ($e) {
+    echo "Anax: Uncaught exception: <p>"
+        . $e->getMessage()
+        . "</p><p>Code: "
+        . $e->getCode()
+        . "</p><pre>"
+        . $e->getTraceAsString()
+        . "</pre>";
 });
-
