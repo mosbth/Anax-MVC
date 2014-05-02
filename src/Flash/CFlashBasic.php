92fe727194d73dd8e63f74b2e072656ba0ca6509
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-05-02 10:08:21 +0200

    Adding flash message

---
+++ src/Flash/CFlashBasic.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Anax\Flash;
+
+/**
+ * Store messages for flashing them to the user as user feedback.
+ *
+ */
+class CFlashBasic
+{
+    /**
+     * Properties
+     *
+     */
+    private $message;
+
+
+
+   /**
+     * Set a message.
+     *
+     * @param string a message.
+     *     
+     * @return void
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+    }
+
+
+
+   /**
+     * Get the message.
+     *
+     * @return void
+     *
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+}
