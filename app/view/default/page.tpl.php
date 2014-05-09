db1393bc84b6b8061927a8e25bdfed07d5b7e1bd
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-05-09 15:19:36 +0200

    enabling to print list of links

--- app/view/default/page.tpl.php
+++ app/view/default/page.tpl.php
@@ -1,3 +1,11 @@
 <h1><?=$title?></h1>
 
 <?=$content?>
+
+<?php if (isset($links)) : ?>
+<ul>
+<?php foreach ($links as $link) : ?>
+<li><a href="<?=$link['href']?>"><?=$link['text']?></li>
+<?php endforeach; ?>
+</ul>
+<?php endif; ?>
