30d143781f5cdda558ea3b79dc507f083a33b3fa
Mikael Roos <mikael.t.h.roos@gmail.com>
2014-10-25 15:45:11 +0200

    added test for navbar anc creating urls in navbar

---
+++ webroot/test/navigation-bar.php
@@ -0,0 +1,46 @@
+<?php 
+/**
+ * app is a Anax frontcontroller.
+ *
+ */
+
+// Get environment & autoloader.
+require __DIR__.'/../config.php';
+
+
+// Create services and inject into the app. 
+$di  = new \Anax\DI\CDIFactoryTest();
+$app = new \Anax\Kernel\CAnax($di);
+
+
+
+// Create the home route
+$app->router->add('*', function () use ($app) {
+
+    $app->theme->setTitle("Testing navigation bar");
+    $app->views->add('default/page', [
+        'title' => "Test navigation bar",
+        'content' => "Testing the navigation bar and linking within it. The urls are just for show and clicking them may result in 404. Review how links are created by seeing the source for the configuration of the navbar in site/config/navbar.php.",
+    ]);
+
+    // Create the navbar as a view
+    $app->theme->addStylesheet('css/navbar.css');
+    $app->views->addCallback(
+        function () use ($app) {
+            return $app->navbar->create();
+        },
+        [],
+        'navbar'
+    );
+
+});
+
+
+
+// Check for matching routes and dispatch to controller/handler of route
+$app->router->handle();
+
+
+
+// Render the page
+$app->theme->render();
