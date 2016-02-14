80aa60b5824b356408e6f5e12ef175fddae6ce10
Fredrik Nilsson <fn@live.se>
2016-02-14 11:58:06 +0000

    Adds framework working with LESS.

---
+++ theme/anax-grid/index.tpl.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html class='no-js' lang='<?=$lang?>'>
+<head>
+<meta charset='utf-8'/>
+<title><?=$title . $title_append?></title>
+<?php if(isset($favicon)): ?><link rel='icon' href='<?=$this->url->asset($favicon)?>'/><?php endif; ?>
+<?php foreach($stylesheets as $stylesheet): ?>
+<link rel='stylesheet' type='text/css' href='<?=$this->url->asset($stylesheet)?>'/>
+<?php endforeach; ?>
+<?php if(isset($style)): ?><style><?=$style?></style><?php endif; ?>
+<script src='<?=$this->url->asset($modernizr)?>'></script>
+<?php // TODO: Add below resource in a better way using the framework?>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+<div id='wrapper'>
+
+<div id='header'>
+<?php if(isset($header)) echo $header?>
+<?php $this->views->render('header')?>
+</div>
+
+<?php if ($this->views->hasContent('navbar')) : ?>
+<div id='navbar'>
+<?php $this->views->render('navbar')?>
+</div>
+<?php endif; ?>
+
+<div id='main'>
+<?php if(isset($main)) echo $main?>
+<?php $this->views->render('main')?>
+</div>
+
+<div id='footer'>
+<?php if(isset($footer)) echo $footer?>
+<?php $this->views->render('footer')?>
+</div>
+
+</div>
+
+<?php if(isset($jquery)):?><script src='<?=$this->url->asset($jquery)?>'></script><?php endif; ?>
+
+<?php if(isset($javascript_include)): foreach($javascript_include as $val): ?>
+<script src='<?=$this->url->asset($val)?>'></script>
+<?php endforeach; endif; ?>
+
+<?php if(isset($google_analytics)): ?>
+<script>
+  var _gaq=[['_setAccount','<?=$google_analytics?>'],['_trackPageview']];
+  (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+  g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+  s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
+<?php endif; ?>
+
+</body>
+</html>
