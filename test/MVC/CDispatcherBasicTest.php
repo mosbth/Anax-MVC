853041bc465b55a71792068061417a2852772900
Dennis Skoko <dennis.skoko@hotmail.com>
2016-05-12 13:16:32 +0200

    Added tests for CDispatcherBasic parameter validator

--- test/MVC/CDispatcherBasicTest.php
+++ test/MVC/CDispatcherBasicTest.php
@@ -44,4 +44,33 @@ class CDispatcherBasicTest extends \PHPUnit_Framework_TestCase
         $disp->setActionName('not-exists');
         $disp->dispatch();
     }
+
+
+
+    /**
+     * Test
+     *
+     * @return void
+     */
+    public function testDispatchParamsValidator()
+    {
+        $di = new \Anax\DI\CDI();
+        $di->set("ErrorController", function () {
+            return new \Anax\MVC\ErrorController();
+        });
+        $disp = new \Anax\MVC\CDispatcherBasic();
+        $disp->setDI($di);
+
+        $disp->setControllerName("Error");
+        $disp->setActionName("statusCode");
+
+        $disp->setParams(["this", "is", "not", "valid"]);
+        $this->assertFalse($disp->isParamsValid());
+
+        $disp->setParams(["very", "valid"]);
+        $this->assertTrue($disp->isParamsValid());
+
+        $disp->setParams();
+        $this->assertTrue($disp->isParamsValid());
+    }
 }
